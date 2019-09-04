75df27adb54b10be16e10e386ac3c05855a5f2be
Sagnik Sarkar <41524773+007Nil@users.noreply.github.com>
2019-09-04 15:56:32 +0530

    stable version
    modify log details

--- Network_monitoring.sh
+++ Network_monitoring.sh
@@ -6,14 +6,14 @@
 ################################################
 #Script Starts HERE!!!
 
-COUNT=30
+COUNT=100
 HASNET=0
 NONET=0
 
 # email report when 
 SUBJECT="Ping failed"
 SUBJECT2="40% Ping Failed"
-EMAILID="Your mail address"
+EMAILIDS="sagnik.sarkar@simpsoftsolutions.com joydeep.bakshi@simpsoftsolutions.com joydeep@simpsoftsolutions.com"
 SUBJECT3="100% Ping Avalable"
 while [ 1 ]
 do
@@ -21,33 +21,37 @@ do
   if [ "$count" = "0" ];  then
     # 100% failed 
     if [ "$NONET" = "0" ]; then
-    echo "Host : All Servers are down (ping failed) at $(date)" | mail -s "$SUBJECT" $EMAILID
+    echo "Host : All Servers are down (ping failed) at $(date)" | mail -s "$SUBJECT" $EMAILIDS
     echo "Mail is send to inform there is no internet"
     HASNET=1
     NONET=1
     fi
   fi
-  if [ "$count" = "20" ];then
+  if [ "$count" -lt "60" ];then
     #40% failed
-    echo "Host : All Servers are Critical (40% ping failed) at $(date)" | mail -s "$SUBJECT2" $EMAILID
+    echo "Host : All Servers are Critical (40% ping failed) at $(date)" | mail -s "$SUBJECT2" $EMAILIDS
     HASNET=1
     fi
-    if [ "$count" = "30" ];then
+    if [ "$count" = "100" ];then
       if [ "$HASNET" = "1" ];then
-         echo "Host : All Servers are running properly (100% ping) at $(date)" | mail -s "$SUBJECT3" $EMAILID
+         echo "Host : All Servers are running properly (100% ping) at $(date)" | mail -s "$SUBJECT3" $EMAILIDS
          HASNET=0
 #   echo "HI"
          NONET=0
       fi 
     fi
 #Generated Logs in logfile
-        if [ "$count" = "30" ];then
+        if [ "$count" = "100" ];then
                 echo " Network is completly Stable at $(date) and All Servers are Up and Running "
+        elif [ "$count" -lt 60  ];then
+                echo "Network is Critical (40% ping failed)  at $(date)"
         else
-                echo "Network is down at $(date)"
+                 echo "Network is Down (100% Ping Failed)  at $(date)"
         fi
     echo " $HASNET is value for HASNET variable for debug purpused "
-    echo "$NONET   is value for NONET variable for debug purpused"
+    echo " $NONET is value for NONET variable for debug purpused"
 done
+
+
 
 
